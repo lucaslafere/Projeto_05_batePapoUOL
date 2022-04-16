f8ff6c775b43bef5683a7dbebec3ecc22e8011c5
Lucas Lafèré <laferelcas@gmail.com>
2022-04-16 12:14:24 -0300

    adicionada funcao de enviar mensagem clicando no icone de enviar msg, apenas mensagem para todos (como pedido nos requisitos). Todos requisitos basicos ja estao cumpridos, faltando melhorar a atualizacao de pagina - que no momento funciona com scroll etc, mas atualiza de um jeito nao muito agradavel visualmente. Nenhum requisito bonus feito ainda (nao envia msg privada) mas recebe apenas mensagem privada corretamente, somente quando os nomes condizem e com layout certo etc

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const msgStatus = document.querySelector(".status");
-const msg = document.querySelectorAll(".caixa-mensagem");
-const msgPrivada = document.querySelector(".reservada");
 const container = document.querySelector(".container");
 
 
@@ -19,20 +16,20 @@ requisicao.then(enviarUsuario);
 requisicao.catch(usuarioInvalido);
 
 
-function enviarUsuario (resposta) {
-    console.log("Enviou usuário corretamente")
+function enviarUsuario(resposta) {
+    console.log("Enviou usuário corretamente");
 }
 
-function usuarioInvalido (error) {
+function usuarioInvalido(error) {
     console.log("Status code: " + error.response.status);
     console.log("Mensagem de erro: " + error.response.data);
     alert("Digite outro nome");
     window.location.reload(true);
 }
 
-setInterval (function (){
-    const statusOnline = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nomeArmazenado);
-    console.log("Você está online")
+setInterval(function () {
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nomeArmazenado);
+    console.log("Você está online");
 }, 5000)
 
 // fim status check usuario
@@ -41,12 +38,16 @@ setInterval (function (){
 
 // adicionando display de mensagens ao body
 
-setInterval (function () {let promiseMsg = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-container.innerHTML = ``;
-promiseMsg.then(tratarMsg);
-promiseMsg.catch(tratarErro);
+setInterval(updateMensagem, 3000);
 
-}, 3000);
+function updateMensagem() {
+    let promiseMsg = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    container.innerHTML = ``;
+    promiseMsg.then(tratarMsg);
+    promiseMsg.catch(tratarErro);
+}
+
+
 
 function tratarMsg(resposta) {
 
@@ -59,7 +60,7 @@ function tratarMsg(resposta) {
         const type = resposta.data[i].type;
 
         adicionarMensagens(time, from, to, text, type);
-        container.lastChild.scrollIntoView()
+        container.lastChild.scrollIntoView();
     }
 }
 
@@ -67,12 +68,16 @@ function tratarMsg(resposta) {
 function adicionarMensagens(time, from, to, text, type) { //funçao que adiciona ao html
     if (type === "status") {
         container.innerHTML +=
-            `<div class="caixa-mensagem ${type}"><p>(${time}) <strong>${from}</strong>
+            `<div class="caixa-mensagem ${type}"><h1>(${time})</h1> <p> <strong>${from}</strong>
          ${text}</p></div>`
     }
-    else {
+    if (type === "private_message" && seuNome === to) {
+        `<div class="caixa-mensagem ${type}"><h1>(${time})</h1> <p> <strong>${from}</strong> para 
+        <strong>${to}: </strong>${text}</p></div>`
+    }
+    else if (type === "message") {
         container.innerHTML +=
-            `<div class="caixa-mensagem ${type}"><p>(${time}) <strong>${from}</strong> para 
+            `<div class="caixa-mensagem ${type}"><h1>(${time})</h1> <p> <strong>${from}</strong> para 
         <strong>${to}: </strong>${text}</p></div>`
     }
 
@@ -88,3 +93,32 @@ function tratarErro(error) {
 
 //fim do display de mensagens do body
 
+
+//envio de mensagem
+function enviarMensagem() {
+    const mensagem = document.querySelector("input").value;
+    const msgObject = {
+        from: seuNome,
+        to: "Todos",
+        text: mensagem,
+        type: "message" // ou "private_message" para o bônus
+    }
+
+    const envioMsg = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", msgObject);
+    envioMsg.then(confirmaMensagem);
+    envioMsg.catch(errorMensagem);
+}
+
+function confirmaMensagem() {
+    console.log("Sua mensagem foi enviada!");
+    updateMensagem();
+    document.querySelector("input").value = '';
+}
+
+function errorMensagem(error) {
+    console.log("Status code: " + error.response.status);
+    console.log("Mensagem de erro " + error.response.data);
+    window.location.reload(true);
+}
+
+//fim do envio de mensagem
